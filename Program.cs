56fdac51364004d7f129cce63fd1302ba0ad02e3
Brette <98058652+brette-0@users.noreply.github.com>
2025-04-02 05:11:29 +0100

    Constant Integer Literal Math Evaluation Working
    Need String Object Handling
    Need Null Coalescion
    Need more tests
    Need Variable Tests
    Need Assignment Code

--- Program.cs
+++ Program.cs
@@ -1,25 +1,217 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Collections.Specialized;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using System.Data;
 
-public static class Tataru {
+using Tataru;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+
+public static class Program {
+    static readonly string OperatorRegex = @"(\d+|\w+|<=|>=|==|!=|\+\+|--|\+=|-=|\*=|/=|\$=|\??=|%=|&=|\|=|\^=|>>=|<<=|<<|>>|<=|>=|==|!=|\+|\-|\*|\/|&|\| |\^|\%|\$|!|#|\\|@|[(){}\[\]|<|>|\|]+)";
+    static readonly HashSet<string> OperatorLUT = [
+        "<", ">", "==", "!=", "<=", ">=", "?", "??", "+", "-", "*", "/", "&", "|", "^",
+        "%", "$", "!", ">>", "<<", ">|", "|<", "++", "--", "+=", "-=", "*=", "/=",
+        "$=", "??=", "%=", "&=", "|=", "^=", ">>=", "<<=", "#", "\\", "@", "(", ")", "[", "]", "{", "}"
+    ];
+
     static bool DebugFile = false;
     static bool ListingFile = false;
 
-    static Targets Target = Targets.NES;
+    static Targets Target = Targets.None;
+
+    // line position at include : file as array of lines split by new line
+    static InterruptableMultiline[] AssemblyFileTree = [];
 
     static string SourceEntryPointFilePath = "";
     static string TargetEntryPointFilePath = "";
 
     // scope[name] = value for all labels
-    static Dictionary<string, Dictionary<string, int?>> LabelDataBas = new();
+    static Dictionary<string, Dictionary<string, int?>> LabelDataBase = [];
+    static string ActiveScope = "root";
+
+
+    static async Task<int> Eval(string expression) {
+        try {
+            return CSharpScript.EvaluateAsync<int>(
+                expression,
+                ScriptOptions.Default.WithImports("System")
+            ).Result;
+        } catch (Exception) {
+            return 0;
+        }
+    }
 
     public static int Main(String[] args) {
+
+        LabelDataBase["root"] = [];                     // create root scope
+
+        string TestString = "2 $ 3";
+        int foo = RequestEvaluate(ref TestString).Response;
+
+
         EXIT_CODES resp = ResolveArguments(ref args);   // digest cli args
         if (resp != 0) return (int)resp;                // if fail, return with fail ctx
 
+        AssemblyFileTree = [.. AssemblyFileTree, new InterruptableMultiline {Index = 0, Lines = File.ReadAllLines(SourceEntryPointFilePath) }];
+
+        //AssemblyFileTree.Add(0, new InterruptableMultiline { 0, File.ReadAllLines(SourceEntryPointFilePath) });  // Index 0 (top/entryhpoint) | Included at 0 (source include) | contents of file
+        Assemble();
         return 0;
     }
 
+    private static EXIT_CODES Assemble() {
+        for (int iter = AssemblyFileTree[^1].Index; iter < AssemblyFileTree[^1].Lines.Length; iter++) {
+            // Request Decode Stage
+            string CurrentString = AssemblyFileTree[^1].Lines[iter];
+
+            // if the line cannot be completed then we assume it has information suceeding it on the following line, concaternate and increment if so
+
+            if (CurrentString.Contains("#include")) {
+                AssemblyFileTree[^1].Index = iter + 1; // register to start after include after include is finished
+
+                string FilePath = "";
+
+                
+
+                // fetch path
+                AssemblyFileTree[0].Index = 0; AssemblyFileTree[0].Lines = File.ReadAllLines(FilePath);
+            }
+        }
+        return EXIT_CODES.OK;
+    }
+
+    private enum Types {
+        e_int,
+        e_str,
+        e_arr,
+        e_err
+    }
+
+    private enum EvaluationLevel {
+        OK,
+        WAIT
+    }
+
+    struct ReconstructSegment {
+        public EvaluationLevel Level;
+        public string Segment;
+        public int Index;
+    }
+
+    private static StatusResponse<int> RequestEvaluate(ref string _Context) {
+        // streamlined approach is the most logically optimal
+        // parenthesis and brackets indicate hierarchy (sort of, brackets are semantically different)
+
+        // split by operator regex
+
+        static string ReplaceExponentiation(string expression) {
+            return Regex.Replace(expression, @"(\d+)\s*\$\s*(\d+)", match =>
+            {
+                int baseNum = int.Parse(match.Groups[1].Value);
+                int exponent = int.Parse(match.Groups[2].Value);
+                return string.Join(" * ", new string[exponent].Select(_ => baseNum.ToString()));
+            });
+        }
+
+        static string[] SplitExpression(string expression) {
+            string[] result = Regex.Split(expression, OperatorRegex);
+
+            result = Array.FindAll(result, s => !string.IsNullOrWhiteSpace(s));
+
+            return result;
+        }
+
+        string[] Context = SplitExpression(_Context);
+
+        ReconstructSegment[] Reconstructs = [];
+        Reconstructs =
+        [ .. Reconstructs,
+            new() {
+                Segment = "",
+                Level = EvaluationLevel.OK,
+            },
+        ];
+
+        bool OperatorClock = false;
+        System.Globalization.NumberStyles ActiveDenotation = System.Globalization.NumberStyles.Number;   // default to decimal denotation alwayss
+
+        static int FetchDenotedNumber(ref string Context, System.Globalization.NumberStyles NumberStyle) {
+            return int.Parse(Context.Trim().ToLower(), NumberStyle);
+        }
+
+        string ExpressionAppend;
+
+        for (int iter = 0; iter < Context.Length; iter++) {
+            if (OperatorLUT.Contains(Context[iter].Trim())) {
+                // if Operator is % or $ it could be binary or hex denotation
+                if (OperatorClock) {
+                    switch (Context[iter].Trim()) {
+                        case "(":
+                            Reconstructs = [.. Reconstructs, new ReconstructSegment { Level = EvaluationLevel.OK, Segment = "" }];
+                            continue;
+
+                        case ")":
+                            if (Reconstructs[^1].Level == EvaluationLevel.WAIT) {
+                                Reconstructs[^2].Level = EvaluationLevel.WAIT;
+                            }
+                            Reconstructs[^2].Segment += $"({Reconstructs[^1].Segment})";
+                            Reconstructs = [.. Reconstructs.Take(Reconstructs.Length - 2)];
+                            break;
+
+                        default:
+                            Reconstructs[^1].Segment += Context[iter].Trim();
+                            break;
+                    }
+                } else {
+                    switch (Context[iter].Trim()) {
+                        case "$":   // hex denote
+                            ActiveDenotation = System.Globalization.NumberStyles.HexNumber;
+                            goto IntParse;
+
+                        case "%":   // bin denote
+                            ActiveDenotation = System.Globalization.NumberStyles.BinaryNumber;
+                            goto IntParse;
+
+                        IntParse:
+                            try {
+                                ExpressionAppend = FetchDenotedNumber(ref Context[++iter], ActiveDenotation).ToString();
+                                ActiveDenotation = System.Globalization.NumberStyles.Number;            // 'reset' - potentially redundant
+                            } catch { goto default; }
+                            Reconstructs[^1].Segment += ExpressionAppend;
+                            break;
+
+                        default:    // SYNTAX ERROR | Two Consecutive Operators
+                            return new StatusResponse<int> {
+                                Status = EXIT_CODES.SYNTAX_ERROR
+                            };
+                    }
+                }
+            } else {
+                try {
+                    int Temp = int.Parse(Context[iter].Trim());
+                    ExpressionAppend = Temp.ToString();
+                    Reconstructs[^1].Segment += ExpressionAppend;
+                } catch {
+                    if (LabelDataBase[ActiveScope].ContainsKey(Context[iter].Trim())) {
+                        if (LabelDataBase[ActiveScope][Context[iter].Trim()] is null) {
+                            Reconstructs[^-1].Segment += Context[iter];
+                        } else {
+                            Reconstructs[^-1].Segment += LabelDataBase[ActiveScope][Context[iter].Trim()];
+                        }
+                    }
+                }
+            }
+            OperatorClock = !OperatorClock;
+        }
+
+
+        return new StatusResponse<int>(EXIT_CODES.OK, Eval(ReplaceExponentiation(Reconstructs[0].Segment)).Result);
+    }
+
     private static EXIT_CODES ResolveArguments(ref String[] args) {
+        if (args.Length == 0) return EXIT_CODES.NOTHING_TO_DO;
         for (int i = 0; i < args.Length; i++) {
             switch (args[i]) {
                 case "-h":
@@ -69,18 +261,33 @@ Commands:
                     return EXIT_CODES.INVALID_ARGUMENT;
             }
         }
-        return 0;
+        return EXIT_CODES.OK;
     }
 
     public enum EXIT_CODES {
         OK,
         INVALID_ARGUMENT,
-        NO_PARAMETER = 2
+        NO_PARAMETER,
+        SYNTAX_ERROR,
+        NOTHING_TO_DO
+    }
+
+    public struct StatusResponse<T> {
+        public EXIT_CODES Status;
+        public T? Response;
+
+        public StatusResponse(EXIT_CODES status, T? response) => (this.Status, this.Response) = (status, response);
     }
 
     public enum Targets {
+        None = -1,          // We permit untargetted code very briefly
         NES = 0,
         FDS = 1
+    }
+
+    struct InterruptableMultiline {
+        public int Index;
+        public string[] Lines;
     }
 
     public class Header {
@@ -91,7 +298,7 @@ Commands:
             raw = [0x46, 0x44, 0x53, 0x1a, (byte)(FDS_DiskAmt / ((FDS_DiskAmt > 65500) ? 65500 : 1))];
         }
 
-        Header(int PRGROM, int CHRROM, bool NametableLayout, bool Battery, bool Trainer, bool AlternativeNametables, int Mapper, int ConsoleType, int SubMapper, int PRGRAM, int EEPROM, int CHRRAM, int CPU, int VsType, int VsPPU, int ExtenedConsoleType, int MiscROMs, int DefaultExpansionDevice) {
+        Header(int PRGROM, int CHRROM, bool NametableLayout, bool Battery, bool Trainer, bool AlternativeNametables, int Mapper, int ConsoleType, int SubMapper, int PRGRAM, int EEPROM, int CHRRAM, int CHRNVRAM, int CPU, int VsType, int VsPPU, int ExtenedConsoleType, int MiscROMs, int DefaultExpansionDevice) {
             Target = Targets.NES;
             if (PRGROM > 0x4000) {
                 // since iNES2 spec allows for 2 bytes of PRGROM in multiples of 16KiB should it be that 0x4000 is specified its unlikely that it refers to ~268MiB so we do it this way
@@ -103,6 +310,26 @@ Commands:
                 CHRROM /= 0x2000;
             }
 
+            // PRGRAM is stored as (0x40 << n)
+            if (PRGRAM >= 0x40) {
+                PRGRAM = (int)MathF.Log(PRGRAM) / 0x40;
+            }
+
+            // EEPROM is stored as (0x40 << n)
+            if (EEPROM >= 0x40) {
+                EEPROM = (int)MathF.Log(EEPROM) / 0x40;
+            }
+
+            // CHRRAM is stored as (0x40 << n)
+            if (CHRRAM >= 0x40) {
+                CHRRAM = (int)MathF.Log(CHRRAM) / 0x40;
+            }
+
+            // CHRNVRAM is stored as (0x40 << n)
+            if (CHRNVRAM >= 0x40) {
+                CHRNVRAM = (int)MathF.Log(CHRNVRAM) / 0x40;
+            }
+
             raw = [
                 0x4e, 0x45, 0x53, 0x1a, // NES<EOF>
                 (byte)(PRGROM & 0xff),
@@ -110,8 +337,8 @@ Commands:
                 (byte)(
                     (NametableLayout       ? 1 : 0) |
                     (Battery               ? 2 : 0) |
-                    (Trainer               ? 1 : 0) |
-                    (AlternativeNametables ? 1 : 0) |
+                    (Trainer               ? 4 : 0) |
+                    (AlternativeNametables ? 8 : 0) |
                     ((Mapper & 0b1111) <<  4)),
                 (byte)(0x08 | CPU | (Mapper & 0xf0)),
                 (byte)((Mapper >> 8) | (SubMapper << 4)),
@@ -130,4 +357,6 @@ Commands:
             ];
         }
     }
+
+
 }
---
+++ VTypes.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tataru {
+    internal class VTypes {
+        internal class num {
+            [Flags]
+            public enum TypeFlags {
+                Endian    = 0x01,
+                Decimal   = 0x02,
+                Signed    = 0x04,
+                Positive  = 0x08,
+            }
+
+            TypeFlags TypeMeta;
+            public int Width;
+            public ushort Offset;
+
+            protected internal num(TypeFlags _TypeFlags, int _Width) {
+                Width = _Width; TypeMeta = _TypeFlags;
+            }
+
+                // u8 holds Width 1 and TypeMeta 0
+                // i8 holds Width 1 and TypeMeta 4
+                // p7 holds Width 1 and TypeMeta 8
+                // n7 holds Width 1 and TypeMeta c
+                // bdn63 holds Width 8 and TypeMeta f
+        }
+
+
+        internal class u8    : num { internal    u8(ushort _Offset) : base(0, 1) { base.Offset = _Offset; } }
+        internal class u16   : num { internal   u16(ushort _Offset) : base(0, 2) { base.Offset = _Offset; } }
+        internal class u24   : num { internal   u24(ushort _Offset) : base(0, 3) { base.Offset = _Offset; } }
+        internal class u32   : num { internal   u32(ushort _Offset) : base(0, 4) { base.Offset = _Offset; } }
+        internal class u64   : num { internal   u64(ushort _Offset) : base(0, 8) { base.Offset = _Offset; } }
+        internal class i8    : num { internal    i8(ushort _Offset) : base(num.TypeFlags.Signed, 1) { base.Offset = _Offset; } }
+        internal class i16   : num { internal   i16(ushort _Offset) : base(num.TypeFlags.Signed, 2) { base.Offset = _Offset; } }
+        internal class i24   : num { internal   i24(ushort _Offset) : base(num.TypeFlags.Signed, 3) { base.Offset = _Offset; } }
+        internal class i32   : num { internal   i32(ushort _Offset) : base(num.TypeFlags.Signed, 4) { base.Offset = _Offset; } }
+        internal class i64   : num { internal   i64(ushort _Offset) : base(num.TypeFlags.Signed, 8) { base.Offset = _Offset; } }
+        internal class p7    : num { internal    p7(ushort _Offset) : base(num.TypeFlags.Positive, 1) { base.Offset = _Offset; } }
+        internal class p15   : num { internal   p15(ushort _Offset) : base(num.TypeFlags.Positive, 2) { base.Offset = _Offset; } }
+        internal class p23   : num { internal   p23(ushort _Offset) : base(num.TypeFlags.Positive, 3) { base.Offset = _Offset; } }
+        internal class p31   : num { internal   p31(ushort _Offset) : base(num.TypeFlags.Positive, 4) { base.Offset = _Offset; } }
+        internal class p63   : num { internal   p63(ushort _Offset) : base(num.TypeFlags.Positive, 8) { base.Offset = _Offset; } }
+        internal class n7    : num { internal    n7(ushort _Offset) : base(num.TypeFlags.Signed | num.TypeFlags.Positive, 1) { base.Offset = _Offset; } }
+        internal class n15   : num { internal   n15(ushort _Offset) : base(num.TypeFlags.Signed | num.TypeFlags.Positive, 2) { base.Offset = _Offset; } }
+        internal class n23   : num { internal   n23(ushort _Offset) : base(num.TypeFlags.Signed | num.TypeFlags.Positive, 3) { base.Offset = _Offset; } }
+        internal class n31   : num { internal   n31(ushort _Offset) : base(num.TypeFlags.Signed | num.TypeFlags.Positive, 4) { base.Offset = _Offset; } }
+        internal class n63   : num { internal   n63(ushort _Offset) : base(num.TypeFlags.Signed | num.TypeFlags.Positive, 8) { base.Offset = _Offset; } }
+        internal class du8   : num { internal   du8(ushort _Offset) : base(num.TypeFlags.Decimal, 1) { base.Offset = _Offset; } }
+        internal class du16  : num { internal  du16(ushort _Offset) : base(num.TypeFlags.Decimal, 2) { base.Offset = _Offset; } }
+        internal class du24  : num { internal  du24(ushort _Offset) : base(num.TypeFlags.Decimal, 3) { base.Offset = _Offset; } }
+        internal class du32  : num { internal  du32(ushort _Offset) : base(num.TypeFlags.Decimal, 4) { base.Offset = _Offset; } }
+        internal class du64  : num { internal  du64(ushort _Offset) : base(num.TypeFlags.Decimal, 8) { base.Offset = _Offset; } }
+        internal class di8   : num { internal   di8(ushort _Offset) : base(num.TypeFlags.Decimal | num.TypeFlags.Signed, 1) { base.Offset = _Offset; } }
+        internal class di16  : num { internal  di16(ushort _Offset) : base(num.TypeFlags.Decimal | num.TypeFlags.Signed, 2) { base.Offset = _Offset; } }
+        internal class di24  : num { internal  di24(ushort _Offset) : base(num.TypeFlags.Decimal | num.TypeFlags.Signed, 3) { base.Offset = _Offset; } }
+        internal class di32  : num { internal  di32(ushort _Offset) : base(num.TypeFlags.Decimal | num.TypeFlags.Signed, 4) { base.Offset = _Offset; } }
+        internal class di64  : num { internal  di64(ushort _Offset) : base(num.TypeFlags.Decimal | num.TypeFlags.Signed, 8) { base.Offset = _Offset; } }
+        internal class dp7   : num { internal   dp7(ushort _Offset) : base(num.TypeFlags.Decimal | num.TypeFlags.Positive, 1) { base.Offset = _Offset; } }
+        internal class dp15  : num { internal  dp15(ushort _Offset) : base(num.TypeFlags.Decimal | num.TypeFlags.Positive, 2) { base.Offset = _Offset; } }
+        internal class dp23  : num { internal  dp23(ushort _Offset) : base(num.TypeFlags.Decimal | num.TypeFlags.Positive, 3) { base.Offset = _Offset; } }
+        internal class dp31  : num { internal  dp31(ushort _Offset) : base(num.TypeFlags.Decimal | num.TypeFlags.Positive, 4) { base.Offset = _Offset; } }
+        internal class dp63  : num { internal  dp63(ushort _Offset) : base(num.TypeFlags.Decimal | num.TypeFlags.Positive, 8) { base.Offset = _Offset; } }
+        internal class dn7   : num { internal   dn7(ushort _Offset) : base(num.TypeFlags.Decimal | num.TypeFlags.Signed | num.TypeFlags.Positive, 1) { base.Offset = _Offset; } }
+        internal class dn15  : num { internal  dn15(ushort _Offset) : base(num.TypeFlags.Decimal | num.TypeFlags.Signed | num.TypeFlags.Positive, 2) { base.Offset = _Offset; } }
+        internal class dn23  : num { internal  dn23(ushort _Offset) : base(num.TypeFlags.Decimal | num.TypeFlags.Signed | num.TypeFlags.Positive, 3) { base.Offset = _Offset; } }
+        internal class dn31  : num { internal  dn31(ushort _Offset) : base(num.TypeFlags.Decimal | num.TypeFlags.Signed | num.TypeFlags.Positive, 4) { base.Offset = _Offset; } }
+        internal class dn63  : num { internal  dn63(ushort _Offset) : base(num.TypeFlags.Decimal | num.TypeFlags.Signed | num.TypeFlags.Positive, 8) { base.Offset = _Offset; } }
+        internal class bu16  : num { internal  bu16(ushort _Offset) : base(num.TypeFlags.Endian, 2) { base.Offset = _Offset; } }
+        internal class bu24  : num { internal  bu24(ushort _Offset) : base(num.TypeFlags.Endian, 3) { base.Offset = _Offset; } }
+        internal class bu32  : num { internal  bu32(ushort _Offset) : base(num.TypeFlags.Endian, 4) { base.Offset = _Offset; } }
+        internal class bu64  : num { internal  bu64(ushort _Offset) : base(num.TypeFlags.Endian, 8) { base.Offset = _Offset; } }
+        internal class bi16  : num { internal  bi16(ushort _Offset) : base(num.TypeFlags.Endian | num.TypeFlags.Signed, 2) { base.Offset = _Offset; } }
+        internal class bi24  : num { internal  bi24(ushort _Offset) : base(num.TypeFlags.Endian | num.TypeFlags.Signed, 3) { base.Offset = _Offset; } }
+        internal class bi32  : num { internal  bi32(ushort _Offset) : base(num.TypeFlags.Endian | num.TypeFlags.Signed, 4) { base.Offset = _Offset; } }
+        internal class bi64  : num { internal  bi64(ushort _Offset) : base(num.TypeFlags.Endian | num.TypeFlags.Signed, 8) { base.Offset = _Offset; } }
+        internal class bp15  : num { internal  bp15(ushort _Offset) : base(num.TypeFlags.Endian | num.TypeFlags.Positive, 2) { base.Offset = _Offset; } }
+        internal class bp23  : num { internal  bp23(ushort _Offset) : base(num.TypeFlags.Endian | num.TypeFlags.Positive, 3) { base.Offset = _Offset; } }
+        internal class bp31  : num { internal  bp31(ushort _Offset) : base(num.TypeFlags.Endian | num.TypeFlags.Positive, 4) { base.Offset = _Offset; } }
+        internal class bp63  : num { internal  bp63(ushort _Offset) : base(num.TypeFlags.Endian | num.TypeFlags.Positive, 8) { base.Offset = _Offset; } }
+        internal class bn15  : num { internal  bn15(ushort _Offset) : base(num.TypeFlags.Endian | num.TypeFlags.Signed | num.TypeFlags.Positive, 2) { base.Offset = _Offset; } }
+        internal class bn23  : num { internal  bn23(ushort _Offset) : base(num.TypeFlags.Endian | num.TypeFlags.Signed | num.TypeFlags.Positive, 3) { base.Offset = _Offset; } }
+        internal class bn31  : num { internal  bn31(ushort _Offset) : base(num.TypeFlags.Endian | num.TypeFlags.Signed | num.TypeFlags.Positive, 4) { base.Offset = _Offset; } }
+        internal class bn63  : num { internal  bn63(ushort _Offset) : base(num.TypeFlags.Endian | num.TypeFlags.Signed | num.TypeFlags.Positive, 8) { base.Offset = _Offset; } }
+        internal class bdu16 : num { internal bdu16(ushort _Offset) : base(num.TypeFlags.Endian | num.TypeFlags.Decimal, 2) { base.Offset = _Offset; } }
+        internal class bdu24 : num { internal bdu24(ushort _Offset) : base(num.TypeFlags.Endian | num.TypeFlags.Decimal, 3) { base.Offset = _Offset; } }
+        internal class bdu32 : num { internal bdu32(ushort _Offset) : base(num.TypeFlags.Endian | num.TypeFlags.Decimal, 4) { base.Offset = _Offset; } }
+        internal class bdu64 : num { internal bdu64(ushort _Offset) : base(num.TypeFlags.Endian | num.TypeFlags.Decimal, 8) { base.Offset = _Offset; } }
+        internal class bdi16 : num { internal bdi16(ushort _Offset) : base(num.TypeFlags.Endian | num.TypeFlags.Decimal | num.TypeFlags.Signed, 2) { base.Offset = _Offset; } }
+        internal class bdi24 : num { internal bdi24(ushort _Offset) : base(num.TypeFlags.Endian | num.TypeFlags.Decimal | num.TypeFlags.Signed, 3) { base.Offset = _Offset; } }
+        internal class bdi32 : num { internal bdi32(ushort _Offset) : base(num.TypeFlags.Endian | num.TypeFlags.Decimal | num.TypeFlags.Signed, 4) { base.Offset = _Offset; } }
+        internal class bdi64 : num { internal bdi64(ushort _Offset) : base(num.TypeFlags.Endian | num.TypeFlags.Decimal | num.TypeFlags.Signed, 8) { base.Offset = _Offset; } }
+        internal class bdp15 : num { internal bdp15(ushort _Offset) : base(num.TypeFlags.Endian | num.TypeFlags.Decimal | num.TypeFlags.Positive, 2) { base.Offset = _Offset; } }
+        internal class bdp23 : num { internal bdp23(ushort _Offset) : base(num.TypeFlags.Endian | num.TypeFlags.Decimal | num.TypeFlags.Positive, 3) { base.Offset = _Offset; } }
+        internal class bdp31 : num { internal bdp31(ushort _Offset) : base(num.TypeFlags.Endian | num.TypeFlags.Decimal | num.TypeFlags.Positive, 4) { base.Offset = _Offset; } }
+        internal class bdp63 : num { internal bdp63(ushort _Offset) : base(num.TypeFlags.Endian | num.TypeFlags.Decimal | num.TypeFlags.Positive, 8) { base.Offset = _Offset; } }
+        internal class bdn15 : num { internal bdn15(ushort _Offset) : base(num.TypeFlags.Endian | num.TypeFlags.Decimal | num.TypeFlags.Signed | num.TypeFlags.Positive, 2) { base.Offset = _Offset; } }
+        internal class bdn23 : num { internal bdn23(ushort _Offset) : base(num.TypeFlags.Endian | num.TypeFlags.Decimal | num.TypeFlags.Signed | num.TypeFlags.Positive, 3) { base.Offset = _Offset; } }
+        internal class bdn31 : num { internal bdn31(ushort _Offset) : base(num.TypeFlags.Endian | num.TypeFlags.Decimal | num.TypeFlags.Signed | num.TypeFlags.Positive, 4) { base.Offset = _Offset; } }
+        internal class bdn63 : num { internal bdn63(ushort _Offset) : base(num.TypeFlags.Endian | num.TypeFlags.Decimal | num.TypeFlags.Signed | num.TypeFlags.Positive, 8) { base.Offset = _Offset; } }
+    }
+}
