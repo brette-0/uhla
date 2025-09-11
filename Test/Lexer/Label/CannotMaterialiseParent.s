0a60c709b26dfa650a180d0db794bad0b7365650
Brette <98058652+brette-0@users.noreply.github.com>
2025-09-11 22:01:20 +0100

    Added Additional Tests

---
+++ Test/Lexer/Label/CannotMaterialiseParent.s
@@ -0,0 +1,2 @@
+﻿foo.Length:
+// while we CAN calculate a length for foo that would satisfy this, that would need to define foo which we cannot do.
---
+++ Test/Lexer/Label/NothingToResolve.s
@@ -0,0 +1,2 @@
+﻿1:
+// The way we resolve these is by performing CPU = (tar) where tar MUST contain one undefined to resolve for.
